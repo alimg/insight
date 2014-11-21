80a016ab317b2e4a9f40a3ad619c834046d026ed
root <root>
2014-11-21 07:17:47 -0500

    hello world default page

--- control-server/webservice/WebService.py
+++ control-server/webservice/WebService.py
@@ -6,6 +6,15 @@ from api import User
 class HelloWorld(restful.Resource):
     def get(self):
         return {'hello': 'world'}
+    
+    def post(self):
+        return {'hello': 'post world'}
+
+    def put(self):
+        return {'hello': 'put world'}
+
+    def delete(self):
+        return {'hello': 'delete world!'}
 
 
 class WebService:
@@ -16,6 +25,7 @@ class WebService:
 
         api.add_resource(User.Login, '/login')
         api.add_resource(User.RegisterUser, '/register')
+	api.add_resource(HelloWorld, '/')
 
     def run(self):
         self.app.run(host='0.0.0.0', port=5000)
