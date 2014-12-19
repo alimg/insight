36f17ffb795a4412b9f515a3386441514e26df1b
fmakdemir <fmakdemir@gmail.com>
2014-12-19 09:48:02 -0500

    insight service additions

--- control-server/webservice/WebService.py
+++ control-server/webservice/WebService.py
@@ -1,32 +1,59 @@
 from flask import Flask
 from flask.ext import restful
 from api import User
+from api import Device
+from flask import request
+import os
+import paho.mqtt.client as mqtt
 
+def send_message(topic="test_nar", message="1"):
+	if len(topic) == 0:
+		topic = "test_nar"
+	os.system("mosquitto_pub -h iot.eclipse.org -t /nar/"+topic+" -m "+str(message))
+	os.system("mosquitto_pub -h iot.eclipse.org -t /nar/android -m okay")
 
 class HelloWorld(restful.Resource):
-    def get(self):
-        return {'hello': 'world'}
-    
-    def post(self):
-        return {'hello': 'post world'}
+	def get(self):
+		print [str(x) for x in request.form.values()]
+		topic = "test_nar"
+		if 'nar_id' in request.args.values():
+			topic = request.args['nar_id']
+		send_message(topic)
+		return {'hello': 'ok-get-p'+topic}
+#		topic = request.args['nar_id']
+#		if len(topic) == 0:
+#			topic = "test_nar"
+#		os.system("mosquitto_pub -h iot.eclipse.org -t /nar/"+topic+" -m 1")
+#		return {'hello': 'ok-get'}
 
-    def put(self):
-        return {'hello': 'put world'}
+	def post(self):
+		print request.method
+		if 'nar_id' in request.form:
+			topic = request.form['nar_id']
+		else:
+			topic = "nar_ptest"
+		send_message(topic)
+		return {'hello': 'ok-post'}
 
-    def delete(self):
-        return {'hello': 'delete world!'}
+	def put(self):
+		return {'hello': 'put world'}
 
+	def delete(self):
+		return {'hello': 'delete world!'}
 
 class WebService:
-    def __init__(self):
-        self.app = Flask(__name__)
-        self.app.debug = True
-        api = restful.Api(self.app)
+	def __init__(self):
+		self.app = Flask(__name__)
+		self.app.debug = True
+		api = restful.Api(self.app)
 
-        api.add_resource(User.Login, '/login')
-        api.add_resource(User.RegisterUser, '/register')
-	api.add_resource(HelloWorld, '/')
+		api.add_resource(User.Login, '/login')
+		api.add_resource(User.RegisterUser, '/register')
+		api.add_resource(HelloWorld, '/')
+		api.add_resource(Device.RegisterInsight, '/register_insight')
+		api.add_resource(Device.PullImage, '/insight/image')
+		api.add_resource(Device.PullSound, '/insight/sound')
 
-    def run(self):
-        self.app.run(host='0.0.0.0', port=5000)
+	def run(self):
+		self.app.run(host='0.0.0.0', port=5000)
 
--- control-server/webservice/api/Device.py
+++ control-server/webservice/api/Device.py
@@ -1,1 +1,74 @@
-__author__ = 'alim'
+import MySQLdb
+from flask.ext import restful
+from flask import request
+from cStringIO import StringIO
+from flask import make_response
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+
+import DBUtil
+import ServerConstants
+import SessionUtil
+
+db = MySQLdb.connect(host=ServerConstants.DB_ADDRESS,
+					 user=ServerConstants.DB_USER,
+					 passwd=ServerConstants.DB_PASSWORD,
+					 db=ServerConstants.DB_NAME)
+
+class RegisterInsight(restful.Resource):
+
+	def post(self):
+		iid = request.form['insight_iid']
+		email = request.form['insight_email']
+		#in_pass = request.form['insight_pass']
+		#password = DBUtil.hash_string(in_id +'|'+ in_pass)
+
+		cursor = db.cursor()
+		cursor.execute('SELECT id, email FROM users WHERE email=\'{}\''.format(email))
+
+		user = cursor.fetchone()
+
+		cursor = db.cursor()
+		sql = 'INSERT INTO `device` (`id`, `userid`) ' \
+				'VALUES (\'{}\', \'{}\')'.format(iid, user[0])
+		print sql
+		cursor.execute(sql)
+
+		db.commit()
+
+		return {'status': '0'}
+
+class PullImage(restful.Resource):
+	def post(self):
+#		iid = request.form['insight_iid']
+#		email = request.form['insight_email']
+		iid="TEST_INSIGHT"
+		email="mail@mail.com"
+
+		img = Image.new("RGB", (512, 512), "white")
+		draw = ImageDraw.Draw(img)
+		# font = ImageFont.truetype(<font-file>, <font-size>)
+		font = ImageFont.truetype("sans-serif.ttf", 48)
+		# draw.text((x, y),"Sample Text",(r,g,b))
+		draw.text((0,   0), "iid: "+iid, (0, 0, 0), font=font)
+		draw.text((0, 128), "email: "+email, (0, 0, 0), font=font)
+		output = StringIO()
+		img.save(output, 'PNG')
+		contents = output.getvalue()
+		output.close()
+		response = make_response(contents)
+		response.headers['Content-Type'] = 'image/png'
+		response.headers['Content-Disposition'] = 'attachment; filename=img.png'
+
+
+
+class PullSound(restful.Resource):
+	def post(self):
+		buf = StringIO()
+		# read audio
+		response = make_response(buf.getvalue())
+		buf.close()
+		response.headers['Content-Type'] = 'audio/wav'
+		response.headers['Content-Disposition'] = 'attachment; filename=sound.wav'
+		return response
