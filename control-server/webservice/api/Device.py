4ddc5c6a8cfdfeaffd15d7cc32298ff065462d20
fmakdemir <fmakdemir@gmail.com>
2014-12-19 10:36:44 -0500

    some fix for dynamic image

--- control-server/webservice/api/Device.py
+++ control-server/webservice/api/Device.py
@@ -1,8 +1,12 @@
 import MySQLdb
+
 from flask.ext import restful
 from flask import request
-from cStringIO import StringIO
 from flask import make_response
+from flask import send_file
+
+from cStringIO import StringIO
+
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
@@ -41,25 +45,22 @@ class RegisterInsight(restful.Resource):
 
 class PullImage(restful.Resource):
 	def post(self):
-#		iid = request.form['insight_iid']
-#		email = request.form['insight_email']
-		iid="TEST_INSIGHT"
-		email="mail@mail.com"
+		iid = request.form['insight_iid']
+		email = request.form['insight_email']
+#		iid="TEST_INSIGHT"
+#		email="mail@mail.com"
 
 		img = Image.new("RGB", (512, 512), "white")
 		draw = ImageDraw.Draw(img)
 		# font = ImageFont.truetype(<font-file>, <font-size>)
-		font = ImageFont.truetype("sans-serif.ttf", 48)
+		font = ImageFont.truetype("/root/insight/control-server/webservice/api/sans-serif.ttf", 48)
 		# draw.text((x, y),"Sample Text",(r,g,b))
 		draw.text((0,   0), "iid: "+iid, (0, 0, 0), font=font)
 		draw.text((0, 128), "email: "+email, (0, 0, 0), font=font)
 		output = StringIO()
 		img.save(output, 'PNG')
-		contents = output.getvalue()
-		output.close()
-		response = make_response(contents)
-		response.headers['Content-Type'] = 'image/png'
-		response.headers['Content-Disposition'] = 'attachment; filename=img.png'
+		output.seek(0)
+		return send_file(output, mimetype='image/png')
 
 
 
@@ -67,8 +68,4 @@ class PullSound(restful.Resource):
 	def post(self):
 		buf = StringIO()
 		# read audio
-		response = make_response(buf.getvalue())
-		buf.close()
-		response.headers['Content-Type'] = 'audio/wav'
-		response.headers['Content-Disposition'] = 'attachment; filename=sound.wav'
-		return response
+		return send_file(buf, mimetype='sound/mp3')
