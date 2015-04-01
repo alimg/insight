355e2c8a8825117de7b4fa0102c1cd0d7937bccc
Fmakdemir <fmakdemir@gmail.com>
2015-04-01 05:07:39 +0300

    images updated

---
+++ inSightIM/CMakeFiles/HumanDetection.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/HumanDetection.dir/HumanDetection.cpp.o"
+  "HumanDetection.pdb"
+  "HumanDetection"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/HumanDetection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
